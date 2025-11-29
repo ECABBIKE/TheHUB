e5c898330a46c4a9451070ce3b15881141557cdf
ECABBIKE <111602892+ECABBIKE@users.noreply.github.com>
2025-11-29 23:47:15 +0100

    Update config.php

--- thehub-v4/gs-v4-backend-mini/core/config.php
+++ thehub-v4/gs-v4-backend-mini/core/config.php
@@ -2,11 +2,10 @@
 // core/config.php
 // Adjust these for your webhotel DB
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'gs_v4_test');
-define('DB_USER', 'db_user');
-define('DB_PASS', 'db_password');
-define('DB_CHARSET', 'utf8mb4');
+define('DB_HOST', env('DB_HOST', 'localhost'));
+define('DB_NAME', env('DB_NAME', 'u994733455_thehub'));
+define('DB_USER', env('DB_USER', 'u994733455_rogerthat'));
+define('DB_PASS', env('DB_PASS', 'staggerMYnagger987!'));
 
 define('BASE_URL', '/gs-v4-backend-mini/public');
 
