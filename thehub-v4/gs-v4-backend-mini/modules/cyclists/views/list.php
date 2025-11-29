986332ad61c7e8c5b5439fc187c5c9f6ea442890
ECABBIKE <111602892+ECABBIKE@users.noreply.github.com>
2025-11-30 00:38:23 +0100

    Delete thehub-v4/gs-v4-backend-mini directory

--- thehub-v4/gs-v4-backend-mini/modules/cyclists/views/list.php
+++
@@ -1,6 +0,0 @@
-<div class="card">
-  <div class="card-header">
-    <h1 class="card-title">Cyclists (placeholder)</h1>
-  </div>
-  <p>This is just a placeholder module. Use the Riders module instead.</p>
-</div>
