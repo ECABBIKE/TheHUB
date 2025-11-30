aaf71524d4cee9dfab9a874160e6e4c4b2434bc9
ECABBIKE <111602892+ECABBIKE@users.noreply.github.com>
2025-11-30 01:17:11 +0100

    Delete thehub-v4/app directory

--- thehub-v4/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>TheHUB V4 – Next.js Test</h1>
-      <a href="/thehub-v4/riders">Visa Riders</a>
-    </main>
-  );
-}
